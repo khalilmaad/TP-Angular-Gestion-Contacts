399f7404ae841444c85b20e9c6828444f2a06bb6
khalilmaad <khalil4567620@gmail.com>
2026-04-22 12:00:25 +0300

    feat: ajout du composant fomulaire-contact et liste-contacts

---
+++ src/app/formulaire-contact/formulaire-contact.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire-contact',
+  imports: [],
+  templateUrl: './formulaire-contact.html',
+  styleUrl: './formulaire-contact.css',
+})
+export class FormulaireContact {}
---
+++ src/app/liste-contacts/liste-contacts.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-contacts',
+  imports: [],
+  templateUrl: './liste-contacts.html',
+  styleUrl: './liste-contacts.css',
+})
+export class ListeContacts {}
