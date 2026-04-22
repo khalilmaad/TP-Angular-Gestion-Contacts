9444f53523e6176ae09425437dbf4663138452d9
khalilmaad <khalil4567620@gmail.com>
2026-04-22 12:27:45 +0300

    test : Les 4 types de Data Binding

--- src/app/formulaire-contact/formulaire-contact.ts
+++ src/app/formulaire-contact/formulaire-contact.ts
@@ -11,4 +11,44 @@ import { CommonModule } from '@angular/common';
 export class FormulaireContact {
   titre: string = 'Formulaire de contact';
   version: string = 'Angular 20';
+  // Propriétés pour l'interpolation
+  prenom: string = 'Ahmed';
+  note: number = 17;
+  notes: number[] = [14, 16, 18, 12, 17];
+  dateAujourdhui: string = new Date().toLocaleDateString('fr-FR');
+
+  imageUrl: string = 'https://picsum.photos/200/100';
+  boutonActif: boolean = false;
+  tailleTexte: number = 18;
+  classeCSS: string = 'alerte-info';
+
+
+  compteur: number = 0;
+  couleurBouton: string = 'gris';
+  texteInput: string = '';
+  incrementer(): void {
+    this.compteur++;
+  }
+  decrementer(): void {
+    if (this.compteur > 0) this.compteur--;
+  }
+
+
+  reinitialiser(): void {
+    this.compteur = 0;
+  }
+  changerCouleur(c: string): void {
+    this.couleurBouton = c;
+  }
+  onFrappe(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.texteInput = input.value;
+  }
+
+
+  // Propriétés liées au formulaire
+  nom: string = '';
+  email: string = '';
+  telephone: string = '';
+  age: number = 0;
 }
